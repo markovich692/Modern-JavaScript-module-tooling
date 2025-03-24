b457667548792a98e1bfed61dc42ff131397296e
Marc Diallo <mark.devguru@outlook.com>
2025-03-23 19:31:43 -0500

    install the leaflet package using npm and creates a .gitignore file that ignores the node_modules folder

---
+++ script.js
@@ -0,0 +1,67 @@
+// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart(`bread`, 5);
+// console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
+
+import shoppingCart from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
+
+add('bread', 6);
+add('cake', 6);
+add('gum', 6);
+
+const getLastPost = async function () {
+  // console.log(`start fetching`);
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+//MODULE PATTERN
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (item, quantity) {
+    cart.push({ item, quantity });
+    // console.log(`${quantity} ${item}(s) added to cart.`);
+  };
+
+  const orderStock = function (item, quantity) {
+    cart.push({ item, quantity });
+    // console.log(`${quantity} ${item}(s) ordered from supplier.`);
+  };
+
+  return {
+    cart,
+    shippingCost,
+    totalPrice,
+    totalQuantity,
+    addToCart,
+  };
+})();
+
+shoppingCart2.addToCart('biscuit', 6);
+shoppingCart2.cart.push(shoppingCart2.totalPrice);
+
+// console.log(shoppingCart2.cart);
+// console.log(shoppingCart2.totalPrice);
+
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([51.5, -0.09])
+  .addTo(map)
+  .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+  .openPopup();
---
+++ shoppingCart.js
@@ -0,0 +1,19 @@
+console.log('Exporting module');
+
+const shippingCost = 20;
+export const cart = [];
+// export { cart };
+
+export const addToCart = function (item, quantity) {
+  cart.push({ item, quantity });
+  console.log(`${quantity} ${item}(s) added to cart.`);
+};
+
+const totalPrice = 200;
+const totalQuantity = 237;
+export { totalPrice, totalQuantity as tq };
+
+export default function (item, quantity) {
+  cart.push({ item, quantity });
+  // console.log(`${quantity} ${item}(s) added to cart.`);
+}
