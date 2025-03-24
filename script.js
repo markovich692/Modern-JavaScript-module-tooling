6cb0690d4a7afe832cb620526c92a2ddebfd04cf
Marc Diallo <mark.devguru@outlook.com>
2025-03-23 20:27:36 -0500

    installs lodash packages

--- script.js
+++ script.js
@@ -53,15 +53,3 @@ shoppingCart2.cart.push(shoppingCart2.totalPrice);
 
 // console.log(shoppingCart2.cart);
 // console.log(shoppingCart2.totalPrice);
-
-let map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-  .openPopup();
